1e12c7d622d956af215c565992952f0230ab5419
Severin Stampler <severin@walt.id>
2022-07-13 12:10:48 +0200

    simple landing page

--- src/main/kotlin/id/walt/idp/rest/IDPRestAPI.kt
+++ src/main/kotlin/id/walt/idp/rest/IDPRestAPI.kt
@@ -4,10 +4,11 @@ import id.walt.idp.oidc.OIDCController
 import id.walt.verifier.backend.VerifierController
 import id.walt.webwallet.backend.rest.RestAPI
 import io.javalin.apibuilder.ApiBuilder
+import io.javalin.http.staticfiles.Location
 
 object IDPRestAPI {
   fun start(bindAddress: String = "localhost", port: Int = 8080) {
-    RestAPI.apiTitle = "walt.id OpenID Provider"
+    RestAPI.apiTitle = "walt.id IDP Kit"
     RestAPI.start(bindAddress, port, IDPAccessManager) {
       ApiBuilder.path("api") {
         ApiBuilder.path("oidc") {
@@ -17,6 +18,10 @@ object IDPRestAPI {
           VerifierController.routes
         }
       }
+    }._conf.addStaticFiles { staticFiles ->
+      staticFiles.hostedPath = "/"
+      staticFiles.location = Location.CLASSPATH
+      staticFiles.directory = "web"
     }
   }
 }
