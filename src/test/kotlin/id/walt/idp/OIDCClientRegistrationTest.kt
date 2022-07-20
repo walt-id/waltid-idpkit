abf91575c1e34fb976f74e628ba5595a4b60e577
Severin Stampler <severin@walt.id>
2022-07-20 16:50:18 +0200

    test bad client registration requests

--- src/test/kotlin/id/walt/idp/OIDCClientRegistrationTest.kt
+++ src/test/kotlin/id/walt/idp/OIDCClientRegistrationTest.kt
@@ -108,4 +108,66 @@ class OIDCClientRegistrationTest: OIDCTestBase() {
     getResp2.statusCode shouldBe HttpCode.UNAUTHORIZED.status
 
   }
+
+  @Test
+  fun testIncompatibleClientRegistration() {
+    val metadata = shouldNotThrowAny { OIDCProviderMetadata.resolve(Issuer(OIDC_URI)) }
+
+    // CREATE
+    val req = ClientRegistrationRequest(metadata.registrationEndpointURI, ClientMetadata().apply {
+      redirectionURI = REDIRECT_URI
+      name = CLIENT_NAME
+      // client_secret_jwt auth currently not supported by IDP Kit
+      tokenEndpointAuthMethod = ClientAuthenticationMethod.CLIENT_SECRET_JWT
+      scope = metadata.scopes
+    }, BearerAccessToken(OIDCManager.getClientRegistrationToken()))
+
+    val resp = req.toHTTPRequest().send()
+
+    resp.statusCode shouldBe HttpCode.BAD_REQUEST.status
+    ClientRegistrationResponse.parse(resp).toErrorResponse().errorObject shouldBe RegistrationError.INVALID_CLIENT_METADATA
+  }
+
+  @Test
+  fun testGetOtherClient() {
+    val metadata = shouldNotThrowAny { OIDCProviderMetadata.resolve(Issuer(OIDC_URI)) }
+
+    // CREATE Client 1
+    val req1 = ClientRegistrationRequest(metadata.registrationEndpointURI, ClientMetadata().apply {
+      redirectionURI = REDIRECT_URI
+      name = CLIENT_NAME
+      tokenEndpointAuthMethod = metadata.tokenEndpointAuthMethods.first()
+      scope = metadata.scopes
+    }, BearerAccessToken(OIDCManager.getClientRegistrationToken()))
+    val resp1 = req1.toHTTPRequest().send()
+    resp1.statusCode shouldBe HttpCode.CREATED.status
+    val registrationResponse1 = ClientRegistrationResponse.parse(resp1).toSuccessResponse()
+
+    // CREATE Client 2
+    val req2 = ClientRegistrationRequest(metadata.registrationEndpointURI, ClientMetadata().apply {
+      redirectionURI = REDIRECT_URI
+      name = CLIENT_NAME
+      tokenEndpointAuthMethod = metadata.tokenEndpointAuthMethods.first()
+      scope = metadata.scopes
+    }, BearerAccessToken(OIDCManager.getClientRegistrationToken()))
+    val resp2 = req2.toHTTPRequest().send()
+    resp2.statusCode shouldBe HttpCode.CREATED.status
+    val registrationResponse2 = ClientRegistrationResponse.parse(resp2).toSuccessResponse()
+
+    // GET // try to get client 2 info with client 1 access token
+
+    val getReq = ClientReadRequest(registrationResponse2.clientInformation.registrationURI, registrationResponse1.clientInformation.registrationAccessToken)
+    val getResp = getReq.toHTTPRequest().send()
+    getResp.statusCode shouldBe HttpCode.FORBIDDEN.status
+  }
+
+  @Test
+  fun testGetNonExistingClient() {
+    // GET // try to get non-existing client info with client 1 access token
+    val clientId = "xyz"
+    val token = OIDCManager.getClientRegistrationToken(clientId)
+    val getReq2 = ClientReadRequest(URI("$OIDC_URI/clients/xyz"), BearerAccessToken(token))
+    val getResp2 = getReq2.toHTTPRequest().send()
+    getResp2.statusCode shouldBe HttpCode.UNAUTHORIZED.status
+  }
 }
