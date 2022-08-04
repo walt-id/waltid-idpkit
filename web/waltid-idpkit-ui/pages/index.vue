6f64c5021a93570463429899ee675908d04321f0
Severin Stampler <severin@walt.id>
2022-08-04 09:36:44 +0200

    fix docu link

--- web/waltid-idpkit-ui/pages/index.vue
+++ web/waltid-idpkit-ui/pages/index.vue
@@ -3,14 +3,14 @@
     <div class="fixed-top">
       <div class="text-right px-3 py-3">
         <a href="/api/swagger" class="menu-link" target="_blank">Swagger</a> |
-        <a href="https://docs.walt.id/v/idp-kit" class="menu-link" target="_blank">Documentation</a> |
+        <a href="https://docs.walt.id/v/idpkit/getting-started/quick-start" class="menu-link" target="_blank">Documentation</a> |
         <a href="https://github.com/walt-id/waltid-idpkit" class="menu-link" target="_blank">GitHub</a>
       </div>
     </div>
     <div class="my-auto">
       <div class="text-white h1">walt.id IDP Kit</div>
       <div class="text-center">
-        <a class="btn btn-info" href="https://docs.walt.id/v/idp-kit" target="_blank">Get Started</a>
+        <a class="btn btn-info" href="https://docs.walt.id/v/idpkit/getting-started/quick-start" target="_blank">Get Started</a>
       </div>
     </div>
   </div>
