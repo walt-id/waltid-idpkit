160eb1a0e5152f65401d284a8557728179f24c5f
SuperBatata <khaled.battiche@esprit.tn>
2023-02-25 12:26:09 +0100

    added near wallet selector modal-ui

--- web/waltid-idpkit-ui/nuxt.config.js
+++ web/waltid-idpkit-ui/nuxt.config.js
@@ -22,7 +22,8 @@ export default {
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [],
+    css: [
+        "@near-wallet-selector/modal-ui/styles.css"   ],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [],
