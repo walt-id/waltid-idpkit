7dce8be095f5636269104f55aa18176562908346
SuperBatata <khaled.battiche@esprit.tn>
2023-03-13 22:50:42 +0100

    deleted unsupported wallets

--- web/waltid-idpkit-ui/pages/connect-wallet.vue
+++ web/waltid-idpkit-ui/pages/connect-wallet.vue
@@ -27,23 +27,11 @@ import {SigningType, signMessage} from '@airgap/beacon-sdk';
 import {verifySignature} from "@taquito/utils";
 import { setupWalletSelector } from "@near-wallet-selector/core";
 import { setupModal } from "@near-wallet-selector/modal-ui";
-import { setupNearWallet } from "@near-wallet-selector/near-wallet";
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupSender } from "@near-wallet-selector/sender";
-import { setupHereWallet } from "@near-wallet-selector/here-wallet";
-import { setupMathWallet } from "@near-wallet-selector/math-wallet";
-import { setupNightly } from "@near-wallet-selector/nightly";
-import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
-import { setupNarwallets } from "@near-wallet-selector/narwallets";
+
 import { setupWelldoneWallet } from "@near-wallet-selector/welldone-wallet";
-import { setupLedger } from "@near-wallet-selector/ledger";
-import { setupNightlyConnect } from "@near-wallet-selector/nightly-connect";
+
 import { setupDefaultWallets } from "@near-wallet-selector/default-wallets";
-import { setupNearFi } from "@near-wallet-selector/nearfi";
-import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
-import { setupOptoWallet } from "@near-wallet-selector/opto-wallet";
-import { setupNeth } from "@near-wallet-selector/neth";
-import { setupXDEFI } from "@near-wallet-selector/xdefi";
+
 
 
 import {Near, KeyPair, utils, connect, keyStores, WalletConnection, InMemorySigner, Signer} from "near-api-js";
@@ -183,30 +171,10 @@ Nonce: ${nonce}`
                   network: "testnet",
                   modules: [
                       ...(await setupDefaultWallets()),
-                      setupNearWallet(),
-                      setupMyNearWallet(),
-                      setupSender(),
-                      setupHereWallet(),
-                      setupMathWallet(),
-                      setupNightly(),
-                      setupMeteorWallet(),
-                      setupNarwallets(),
+
                       setupWelldoneWallet(),
-                      setupLedger(),
-                      setupNearFi(),
-                      setupCoin98Wallet(),
-                      setupOptoWallet(),
-                      setupNeth(),
-                      setupXDEFI(),
-                      setupNightlyConnect({
-                          url: "wss://relay.nightly.app/app",
-                          appMetadata: {
-                              additionalInfo: "",
-                              application: "NEAR Wallet Selector",
-                              description: "idpkit  used by waltid",
-                              icon: "https://near.org/wp-content/uploads/2020/09/cropped-favicon-192x192.png",
-                          },
-                      }),
+
+
                   ],
               });
 
