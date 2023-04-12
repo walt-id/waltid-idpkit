61be050845b964b9ef2f7a71e8df84f2f4e5b00d
KBurgmann <kb@gatgetron.net>
2023-04-12 10:03:23 +0200

    Added nuxt proxy

--- web/waltid-idpkit-ui/nuxt.config.js
+++ web/waltid-idpkit-ui/nuxt.config.js
@@ -37,7 +37,8 @@ export default {
     modules: [
         // https://go.nuxtjs.dev/bootstrap
         'bootstrap-vue/nuxt',
-        '@nuxtjs/axios'
+        '@nuxtjs/axios',
+        "@nuxtjs/proxy"
     ],
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
